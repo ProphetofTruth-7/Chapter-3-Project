1a1392d7b64e8dabdde25de5f0988098b054b6ad
Malakye Brown <prophetoftruth07@gmail.com>
2025-10-15 09:51:29 -0400

    Full completion

--- Chapter 3 Project.cpp
+++ Chapter 3 Project.cpp
@@ -1,9 +1,27 @@
 // Comment
 
 #include <iostream>
+#include <random>
 using namespace std;
 
 int main()
 {
-    cout << "Tutor!" << endl;
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dist(1, 1000);
+    int answer;
+    int random1 = dist(gen);
+    int random2 = dist(gen);
+
+    cout << "Hello! Please perform this problem and press enter when you're finished: " << random1 << " + " << random2 << endl;
+    cin >> answer;
+    cin.get();
+    if (answer == (random1 + random2)) {
+        cout << "Right!" << endl;
+    } else {
+        cout << "Incorrect! The answer is actually " << random1 + random2 << endl;
+    }
+
+    return 0;
 }
