b98e63d667a5c41594a782fe1764de5694b79516
Malakye Brown <prophetoftruth07@gmail.com>
2025-10-15 10:31:57 -0400

    testA

--- Chapter 3 Project.cpp
+++ Chapter 3 Project.cpp
@@ -1,9 +1,25 @@
 // Comment
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    cout << "Interest!" << endl;
+    double interestRate;
+    double initialMoney;
+    double interestAccrued;
+    int compoundRate;
+
+    cout << "Hi! Please enter your account's Interest Rate(as a decimal) and press enter!" << endl;
+    cin >> interestRate;
+    cout << "Cool! Now enter how often your Principal is compounded per year!" << endl;
+    cin >> compoundRate;
+    cout << "Finally, enter your initial Principal!" << endl;
+    cin >> initialMoney;
+    interestAccrued = initialMoney * (pow((1+(interestRate / compoundRate)), compoundRate));
+
+    cout << interestRate << " & " << compoundRate << " & " << initialMoney << " & " << interestAccrued << endl;
+
+    return 0;
 }
